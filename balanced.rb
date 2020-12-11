d9f3166b4f8b5aa196497abc039f8d9d7d1593e2
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-11 14:51:48 -0400

    First test case official passed

--- balanced.rb
+++ balanced.rb
@@ -1,10 +1,37 @@
 
 class Balanced
     def getBalance(string)
-        if(string === "()")
-            return true
-        else
+        array = string.split('')
+        i = 0
+        num = array.length
+
+        balanceP = 0 #()
+        balanceSB = 0 #[]
+        balanceB = 0 #{}
+
+        while i < num
+            case array[i]
+            when "("
+                balanceP+=1
+            when ")"
+                balanceP-=1
+            when "["
+                balanceSB+=1
+            when "]"
+                balanceSB-=1
+            when "{"
+                balanceB+=1
+            when "}"
+                balanceB-=1
+              else
+            end
+            i+=1
+        end
+        
+        if(balanceP != 0 or balanceSB != 0 or balanceB != 0)
             return false
+        else
+            return true
         end
     end
 end
