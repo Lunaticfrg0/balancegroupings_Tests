4076df05064d96a6e28b1ad202f4cd929c014801
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-12 12:42:13 -0400

    Initial First test case passed

--- spec/test.rb
+++ spec/test.rb
@@ -1,37 +1,9 @@
 require_relative "../balanced"
 
 RSpec.describe Balanced do
-  describe '"()" => true' do
-    it 'returns true to input: ()' do
-      expect(Balanced.new.getBalance("()")).to eq(true)
-    end
-  end
-end
-RSpec.describe Balanced do
-  describe '"{[)" => false' do
-    it 'returns false to input: {[)' do
-      expect(Balanced.new.getBalance("{[)")).to eq(false)
-    end
-  end
-end
-RSpec.describe Balanced do
-  describe '"a+(c+b)" => true' do
-    it 'returns true to input: a+(c+b)' do
-      expect(Balanced.new.getBalance("a+(c+b)")).to eq(true)
-    end
-  end
-end
-RSpec.describe Balanced do
-  describe '"({[]})" => true' do
-    it 'returns true to input: ({[]})' do
-      expect(Balanced.new.getBalance("({[]})")).to eq(true)
-    end
-  end
-end
-RSpec.describe Balanced do
-  describe '"}" => false' do
-    it 'returns false to input: }' do
-      expect(Balanced.new.getBalance("}")).to eq(false)
+  describe '"(" => false' do
+    it 'returns false to "("' do
+      expect(Balanced.new.getBalance("(")).to eq(false)
     end
   end
 end
