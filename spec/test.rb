5f83a4cbde489dc3f7a6ceadab4496a6351728ab
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-12 12:49:36 -0400

    Second test a+[b+c] case passed

--- spec/test.rb
+++ spec/test.rb
@@ -7,3 +7,10 @@ RSpec.describe Balanced do
     end
   end
 end
+RSpec.describe Balanced do
+  describe '"a+[b+c]" => true' do
+    it 'returns true to "a+[b+c]"' do
+      expect(Balanced.new.getBalance("a+[b+c]")).to eq(true)
+    end
+  end
+end
